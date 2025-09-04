503972cbf5a9d01d6e06ffa6ad1e6d2ba840402b
C5ISR\Michael.P.Spinelli <michael.p.spinelli6.civ@army.mil>
2025-09-04 15:21:35 -0400

    removed cdata from symbolID and wasClipped and those shouldn't ever have special characters

--- src/main/ts/armyc2/c5isr/web/render/MultiPointHandler.ts
+++ src/main/ts/armyc2/c5isr/web/render/MultiPointHandler.ts
@@ -1929,8 +1929,8 @@ export class MultiPointHandler {
         kml += ("<visibility>1</visibility>");
         kml += ("<description>" + cdataStart + description + cdataEnd + "</description>");
         kml += ("<ExtendedData>");
-        kml += ("<Data name=\"symbolID\"><value>" + cdataStart + symbolCode + cdataEnd + "</value></Data>");
-        kml += ("<Data name=\"wasClipped\"><value>" + cdataStart + wasClipped + cdataEnd + "</value></Data>");
+        kml += ("<Data name=\"symbolID\"><value>" + symbolCode + "</value></Data>");
+        kml += ("<Data name=\"wasClipped\"><value>" + wasClipped + "</value></Data>");
         kml += ("</ExtendedData>");
         for (let i: int = 0; i < len; i++) {
             let shapesToAdd: string = MultiPointHandler.ShapeToKMLString(shapes[i], ipc, normalize);
