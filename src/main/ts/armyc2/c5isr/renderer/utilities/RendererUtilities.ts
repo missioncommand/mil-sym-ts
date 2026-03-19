f44d06776e053eac31a2ab90eb3da52520346970
C5ISR\Michael.P.Spinelli <michael.p.spinelli6.civ@army.mil>
2026-03-19 14:01:05 -0400

    fixed space debris outline and adjust space debris linewidth again

--- src/main/ts/armyc2/c5isr/renderer/utilities/RendererUtilities.ts
+++ src/main/ts/armyc2/c5isr/renderer/utilities/RendererUtilities.ts
@@ -446,12 +446,15 @@ export class RendererUtilities {
 
 
             }
-            //key terrain
-            if (symbolID.length >= 20 &&
-                SymbolUtilities.getBasicSymbolID(symbolID) === "25132100" &&
-                SymbolID.getVersion(symbolID) >= SymbolID.Version_2525E) {
-                defaultStrokeColor = "#800080";
-            }
+            
+            if(symbolID.length >= 20)
+                {
+                    if(SymbolUtilities.getBasicSymbolID(symbolID)=="25132100" && //key terrain
+                            SymbolID.getVersion(symbolID) >= SymbolID.Version_2525E)
+                        defaultStrokeColor = "#800080";
+                    else if(isOutline && SymbolUtilities.getBasicSymbolID(symbolID).startsWith("2535"))//space debris doesn't change color
+                        defaultStrokeColor = "black";
+                }
             returnSVG = returnSVG.replaceAll("stroke=\"" + defaultStrokeColor + "\"", "stroke=\"" + hexStrokeColor + "\"" + strokeOpacity);
             returnSVG = returnSVG.replaceAll("fill=\"" + defaultStrokeColor + "\"", "fill=\"" + hexStrokeColor + "\"" + fillOpacity);
         }
