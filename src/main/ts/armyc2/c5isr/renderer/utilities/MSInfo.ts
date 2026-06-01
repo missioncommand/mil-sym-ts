d569af97f2b6f2e49e79574ddd00332f9cb17c39
C5ISR\Michael.P.Spinelli <michael.p.spinelli6.civ@army.mil>
2026-06-01 13:24:31 -0400

    Fix for reported point counts for Line26 & Line26 (symbols like Screen & Seize)

--- src/main/ts/armyc2/c5isr/renderer/utilities/MSInfo.ts
+++ src/main/ts/armyc2/c5isr/renderer/utilities/MSInfo.ts
@@ -917,13 +917,14 @@ export class MSInfo {
                 break;
             }
 
-            case DrawRules.LINE26:
-            case DrawRules.LINE27: {
-                points[0] = 3;
-                if (version >= SymbolID.Version_2525E) 
-                    points[1] = 4;
-                else 
-                    points[1] = 3;
+            case DrawRules.LINE26: //like Screen 25342203
+            case DrawRules.LINE27: //like Seize 25342300
+            {
+                if (version >= SymbolID.Version_2525E) //always 4 points
+                    points[0] = 4;
+                else //3 or 4 points total
+                    points[0] = 3;
+                points[1] = 4;
                 break;
             }
 
